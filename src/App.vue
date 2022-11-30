b2f98164e813165a8a1d0e9014673d0d3a1b842c
Mayank Agarwal <arenmayank20@gmail.com>
2022-11-30 16:42:50 +0530

    Sweet-Dish ready

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld";
+import Dashboard from "./components/Dashboard";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+    Dashboard
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: black;
+}
+body {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+</style>
